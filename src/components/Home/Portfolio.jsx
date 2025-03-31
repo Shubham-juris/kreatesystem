67f0593a3ba6f0d2585d13c234947176ea895099
ashishjuris <ashish.juris@gmail.com>
2025-03-31 16:24:12 +0530

    xcs

--- src/components/Home/Portfolio.jsx
+++ src/components/Home/Portfolio.jsx
@@ -1,27 +1,39 @@
 import React, { useState, useEffect } from "react";
-import { Box, Typography, IconButton } from "@mui/material";
+import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
 import { ArrowBack, ArrowForward } from "@mui/icons-material";
 import SocialImage from "../../assets/socialimg.png";
 import MarketingImage from "../../assets/Marketingimg.png";
 import SupportImage from "../../assets/Supportimg.png";
 import CRMImage from "../../assets/webimg.png";
 import ExpertTeamImg from "../../assets/DigitalMarketingImg/Img1.jpg";
-import InnovationImg  from "../../assets/DigitalMarketingImg/Img2.jpg";
-import  VersatilityImg from "../../assets/DigitalMarketingImg/img3.jpg";
+import InnovationImg from "../../assets/DigitalMarketingImg/Img2.jpg";
+import VersatilityImg from "../../assets/DigitalMarketingImg/img3.jpg";
 import ConsistencyImg from "../../assets/DigitalMarketingImg/img4.jpg";
 import QualityImg from "../../assets/DigitalMarketingImg/Img2.jpg";
 import TailoredOfferingsImg from "../../assets/DigitalMarketingImg/img5.jpg";
 
-
-const images = [SocialImage,ExpertTeamImg,TailoredOfferingsImg,InnovationImg, MarketingImage,QualityImg,VersatilityImg, SupportImage,ConsistencyImg, CRMImage,VersatilityImg];
+const images = [
+  SocialImage,
+  ExpertTeamImg,
+  TailoredOfferingsImg,
+  InnovationImg,
+  MarketingImage,
+  QualityImg,
+  VersatilityImg,
+  SupportImage,
+  ConsistencyImg,
+  CRMImage,
+  VersatilityImg,
+];
 
 const Portfolio = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const isMobile = useMediaQuery("(max-width: 600px)");
 
   useEffect(() => {
     const interval = setInterval(() => {
       handleNext();
-    }, 3000); // Auto-slide every 5 seconds
+    }, 3000);
     return () => clearInterval(interval);
   }, [currentIndex]);
 
@@ -33,18 +45,16 @@ const Portfolio = () => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
   };
 
- 
   const getVisibleImages = () => {
     return [
-      images[(currentIndex - 1 + images.length) % images.length], 
-      images[currentIndex % images.length], 
-      images[(currentIndex + 1) % images.length], 
+      images[(currentIndex - 1 + images.length) % images.length],
+      images[currentIndex % images.length],
+      images[(currentIndex + 1) % images.length],
     ];
   };
 
   return (
     <Box sx={{ textAlign: "center", bgcolor: "#fff", p: 4, position: "relative" }}>
-     
       <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", mb: 3 }}>
         <Box sx={{ flexGrow: 1, height: "2px", bgcolor: "#8B8B5E" }} />
         <Typography variant="h5" sx={{ fontWeight: "bold", color: "#8B8B5E", mx: 2 }}>
@@ -53,59 +63,50 @@ const Portfolio = () => {
         <Box sx={{ flexGrow: 1, height: "2px", bgcolor: "#8B8B5E" }} />
       </Box>
 
-      {/* Image Container */}
-      <Box sx={{ 
-        display: "flex", 
-        alignItems: "center", 
-        justifyContent: "center", 
+      <Box sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
         position: "relative",
-        overflow: "hidden", 
-        width: "100%"
+        overflow: "hidden",
+        width: "100%",
       }}>
-        {/* Previous Button */}
-        <IconButton 
-          onClick={handlePrev} 
-          sx={{ position: "absolute", left: 10, zIndex: 2 }}
-        >
+        <IconButton onClick={handlePrev} sx={{ position: "absolute", left: 10, zIndex: 2 }}>
           <ArrowBack fontSize="large" />
         </IconButton>
 
-        {/* Images */}
-        <Box sx={{ 
-          width: "80%", 
-          display: "flex", 
-          justifyContent: "center", 
+        <Box sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
           alignItems: "center",
-          gap: 2 
+          gap: 2,
         }}>
           {getVisibleImages().map((img, index) => (
-            <Box 
-              key={index} 
+            <Box
+              key={index}
               sx={{
-                width: index === 1 ? "80%" : "30%", 
+                width: index === 1 ? (isMobile ? "90%" : "60%") : "30%",
                 opacity: index === 1 ? 1 : 0.6,
                 transform: index === 1 ? "scale(1.1)" : "scale(0.9)",
                 transition: "all 0.5s ease-in-out",
               }}
             >
-              <img 
-                src={img} 
-                alt="Portfolio" 
-                style={{ 
-                  width: "100%", 
-                  height: "300px", 
-                  objectFit: "cover", 
-                }} 
+              <img
+                src={img}
+                alt="Portfolio"
+                style={{
+                  width: "100%",
+                  height: isMobile ? "200px" : "300px",
+                  objectFit: "cover",
+                  borderRadius: "10px",
+                }}
               />
             </Box>
           ))}
         </Box>
 
-        {/* Next Button */}
-        <IconButton 
-          onClick={handleNext} 
-          sx={{ position: "absolute", right: 10, zIndex: 2 }}
-        >
+        <IconButton onClick={handleNext} sx={{ position: "absolute", right: 10, zIndex: 2 }}>
           <ArrowForward fontSize="large" />
         </IconButton>
       </Box>
